787c1863d82aba311843bbb6c50debe58ed691f5
Federico Walovnik <federicowalovnik@Federicos-MacBook-Pro-2.local>
2023-03-08 11:37:47 -0300

    change firebase api key

--- creds.js
+++ creds.js
@@ -1,16 +1,18 @@
 const credentials = {
-  "type": "service_account",
-  "project_id": "confort-life",
-  "private_key_id": process.env.FIREBASE_API_KEY_ID,
-  "private_key": `-----BEGIN PRIVATE KEY-----${process.env.FIREBASE_API_KEY.replace(/\\n/g, '\n')}-----END PRIVATE KEY-----\n`,
-  "client_email": "firebase-adminsdk-h1cp5@confort-life.iam.gserviceaccount.com",
-  "client_id": "101771906745101125234",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-h1cp5%40confort-life.iam.gserviceaccount.com"
+  type: 'service_account',
+  project_id: 'confort-life',
+  private_key_id: process.env.FIREBASE_API_KEY_ID,
+  private_key: process.env.FIREBASE_API_KEY,
+  client_email:
+    'firebase-adminsdk-h1cp5@confort-life.iam.gserviceaccount.com',
+  client_id: '101771906745101125234',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url:
+    'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-h1cp5%40confort-life.iam.gserviceaccount.com'
 }
-
 
 module.exports = {
   credentials
